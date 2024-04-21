a71486628680b8f77c389445a0d336bc0a64e5c4
bfloyd14 <brendanflo1425@gmail.com>
2024-04-20 23:28:31 -0400

    add show.ejs, show router and controller.

--- controllers/budgets.js
+++ controllers/budgets.js
@@ -46,7 +46,7 @@ function show(req, res){
   .then(budget =>{
     res.render('budgets/show',{
       budget,
-      title: 'Budget Details'
+      title: 'Your Budget'
     })
   })
   .catch(err =>{
